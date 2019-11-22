2826736d4fccbacca9aa4ad443f5cb745089e97b
T0hsakaRin <1023770070@qq.com>
2019-11-22 16:19:11 +0000

    Deploying to gh-pages from master b7463c0505be9f7fd833323338472b1cfb42edc2

---
+++ precache-manifest.6e9c9667c0260e2c887ccd5e0d6ea6b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acfd46ae7d8e0f4d24a8707237d88d98",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "263c8276837a501fd058",
+    "url": "/github-actions-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d2718bbd4f8fb500d63e",
+    "url": "/github-actions-demo/static/js/2.2f60c060.chunk.js"
+  },
+  {
+    "revision": "263c8276837a501fd058",
+    "url": "/github-actions-demo/static/js/main.14bba5cf.chunk.js"
+  },
+  {
+    "revision": "dfb417f264dd525bbb87",
+    "url": "/github-actions-demo/static/js/runtime~main.553754d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
